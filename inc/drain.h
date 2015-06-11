0447625a1f6fdbfb48947c955ab91774997755a7
Eric Ponce <eaponce@mit.edu>
2015-06-11 18:41:32 -0400

    Updated Drain SM to follow charge sm template

--- inc/drain.h
+++ inc/drain.h
@@ -2,14 +2,14 @@
 #define _DRAIN_H_
 
 #include "state_types.h"
+#include "a123mbb.h"
 
 
 #define BCM_DRAIN_ALLOWED(pack_v_min) (pack_v_min > MIN_CELL_V)
 
-typedef enum {DRAIN_DRAINING, DRAIN_INIT, DRAIN_EXIT} DRAIN_MODE_T;
-typedef enum {DRAIN_OK, DRAIN_ERROR, DRAIN_EXITED} DRAIN_STATUS_T;
+typedef enum {DRAIN_DRAINING, DRAIN_INIT, DRAIN_OFF, DRAIN_DONE} DRAIN_MODE_T;
 
-DRAIN_STATUS_T Drain_Step(PACK_STATE_T*, MODE_T, OUTPUT_STATE_T*);
+ERROR_T Drain_Step(PACK_STATE_T*, MODE_REQUEST_T, OUTPUT_STATE_T*);
 DRAIN_MODE_T Drain_GetMode(void);
 
 #endif
--- src/drain.c
+++ src/drain.c
@@ -1,9 +1,9 @@
 #include "drain.h"
 
-static DRAIN_MODE_T mode = DRAIN_INIT;
+static DRAIN_MODE_T mode = DRAIN_OFF;
 
 //Progresses state of DRAIN state machine
-DRAIN_STATUS_T Drain_Step(PACK_STATE_T* pack_state, MODE_T req_mode, OUTPUT_STATE_T* os){
+ERROR_T Drain_Step(PACK_STATE_T* pack_state, MODE_REQUEST_T req_mode, OUTPUT_STATE_T* out_state){
 	// switch(req_mode){
 	// 	case IDLE:
 	// 		os->close_contactors = false;
@@ -29,6 +29,51 @@ DRAIN_STATUS_T Drain_Step(PACK_STATE_T* pack_state, MODE_T req_mode, OUTPUT_STAT
 	// 		}
 	// 		break;
 	// }
+
+	switch (req_mode) {
+		case REQ_CHARGING:
+			// If starting from not charging, go to init, else stay where you are
+			mode = DRAIN_OFF;
+			out_state->close_contactors = false;
+			break;
+		case REQ_DRAINING:
+			// Move to exit state
+			mode = (mode == DRAIN_OFF) ? DRAIN_INIT : mode;
+			break;
+		case REQ_IDLE:
+			// Move to exit state
+			mode = DRAIN_OFF;
+			out_state->close_contactors = false;
+			break;
+		case REQ_NONE:
+			// Chill
+			break;
+	}
+	out_state->brusa_mVolts = 0;
+	out_state->brusa_cAmps = 0;
+	out_state->balance_mVolts = BCM_BALANCE_OFF;
+	switch(mode) {
+		case DRAIN_OFF:
+			out_state->close_contactors = false;
+			break;
+		case DRAIN_INIT:
+			// Start Draining
+			if (!pack_state->contactors_closed) {
+				out_state->close_contactors = true;
+			} else {
+				mode = DRAIN_DRAINING;
+			}
+			break;
+		case DRAIN_DRAINING:
+			// Monitor voltages
+			// Move to Drain_Done if illegal voltage
+			break;
+		case DRAIN_DONE:
+			out_state->close_contactors = false;
+			break;
+	}
+
+	return ERROR_NONE;
 }
 
 
